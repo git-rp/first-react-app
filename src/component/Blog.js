f03bb9e407b46506290c79141a35f5662f563d1d
git-rp <patilrahul000@gmail.com>
2022-09-25 13:51:25 +0530

    blog array and maps

--- src/component/Blog.js
+++ src/component/Blog.js
@@ -1,16 +1,39 @@
-import React from 'react';
-import '../App.css';
+import React from "react";
+import "../App.css";
 function Blog() {
-    const blog =  {
-        title:"My struggles",
-        description :"This is blog description"
-    }
+  const blog = {
+    title: "My struggles",
+    description: "This is blog description",
+  };
 
-  return (
-      <div className='BlogCard'>
-          
-          <h3>{blog.title}</h3><p>{blog.description}</p></div>
-  )
+  const blogArr = [
+    {
+      id: 1,
+      title: "My struggles",
+      description: "This is blog description",
+    },
+    {
+      id: 2,
+      title: "My struggles 1",
+      description: "This is blog description 1",
+    },
+    {
+      id: 3,
+      title: "My struggles 2",
+      description: "This is blog description 2",
+    },
+  ];
+
+  const blogCards = blogArr.map((item, pos) => {
+    console.log(item);
+    return (
+      <div className="BlogCard" key={item.id}>
+        <h3>{item.title}</h3>
+        <p>{item.description}</p>
+      </div>
+    );
+  });
+  return <div>{blogCards}</div>;
 }
 
-export default Blog
+export default Blog;
